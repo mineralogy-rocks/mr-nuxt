afc96626a71651c982e5f22bdda94e815a1af085
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2022-03-31 00:18:49 +0200

    main page WIP

--- components/layout/toolbar.vue
+++ components/layout/toolbar.vue
@@ -2,9 +2,9 @@
     <header class="bg-black sticky min-h-3 top-0 w-full shadow-xl">
         <div class="mx-auto">
             <div class="py-2 mx-4 lg:mx-2">
-                <nav class="relative flex items-center justify-around text-sm sm:text-lg">
+                <nav class="relative flex items-center justify-around text-sm sm:text-lg md:text-xl">
                     <ul class="flex text-white font-semibold">
-                        <n-link :to="'/'">
+                        <n-link class="hover:underline" :to="'/'">
                             <div class="flex flex-row">
                                 <div class="bg-white mr-0.5 text-center self-center">
                                     <span class="text-black">M</span>
@@ -14,9 +14,9 @@
                         </n-link>
                     </ul>
                     <ul class="flex text-white font-semibold mr-4 space-x-2 sm:space-x-6 md:space-x-10">
-                        <n-link :to="'/explore'">Explore</n-link>
-                        <n-link :to="'/about'">About</n-link>
-                        <n-link :to="'/contact'">Contact us</n-link>
+                        <n-link class="hover:underline" :to="'/explore'">Explore</n-link>
+                        <n-link class="hover:underline" :to="'/about'">About</n-link>
+                        <n-link class="hover:underline" :to="'/contact'">Contact us</n-link>
                     </ul>
                 </nav>
             </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
     <header>
-        <div class="flex items-center mt-20">
-            <h1 class="font-black sm:text-8xl text-6xl mx-auto">
+        <div class="flex items-center mt-24">
+            <h1 class="font-black text-6xl sm:text-7xl md:text-8xl mx-auto">
                 Explore. </br>
                 Extract. </br>
                 Research.
             </h1>
+        </div>
+
+        <div class="relative mx-2 mt-14 md:mt-20">
+            <div class="flex justify-center">
+                <button type="button" class="bg-black hover:bg-gray-700 px-4 sm:px-5 md:px-6 py-1.5 sm:py-2 text-sm leading-5 rounded font-semibold text-white drop-shadow-sm">
+                    Start Exploring
+                </button>
+            </div>
+
+            <p class="text-sm text-gray-500 text-center mt-7">
+                Mineralogy.rocks provide seamless and simple way to access and filter mineralogical and related data. </br>
+                Our platform is designed both for researchers and developers.
+            </p>
         </div>
     </header>
 </template> 

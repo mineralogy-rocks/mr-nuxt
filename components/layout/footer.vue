0b8d81de16a7d52e785ce8107f3d277f97e70913
Liubomyr Gavryliv <liubomyr.gavryliv@gmail.com>
2022-04-07 13:01:47 +0200

    main page WIP

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,8 +1,8 @@
 <template>
-    <header class="bg-black min-h-3 bottom-0 z-20 w-full shadow-xl">
-        <div class="mx-auto">
+    <footer>
+        <div class="bg-black min-h-4 bottom-0 z-20 w-full shadow-xl mt-20 mx-auto">
             <div class="py-2 mx-4 lg:mx-2">
-                <nav class="flex items-center justify-around text-sm sm:text-lg md:text-xl">
+                <!-- <nav class="flex items-center justify-around text-sm sm:text-lg md:text-xl">
                     <ul class="flex text-white font-semibold">
                         <n-link class="underline-animation" :to="'/'">
                             <div class="flex flex-row">
@@ -18,10 +18,16 @@
                         <n-link class="underline-animation" :to="'/about'">About</n-link>
                         <n-link class="underline-animation" :to="'/contact'">Contact us</n-link>
                     </ul>
-                </nav>
+                </nav> -->
+                <div class="flex flex-row justify-center text-white text-xs font-bold">
+                    <div class="bg-white mr-0.5 text-center self-center">
+                        <span class="text-black">M</span>
+                    </div>
+                    <span>Copyright of mineralogy.rocks (2022). All rights reserved.</span>
+                </div>
             </div>
         </div>
-    </header>
+    </footer>
 </template>
 
 <script>
@@ -32,6 +38,6 @@
 
 <style lang="scss" scoped>
     .underline-animation {
-        @apply relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300;
+        @apply relative after:absolute after:bg-gray-400 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300;
     }
 </style>
